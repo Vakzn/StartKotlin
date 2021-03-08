2dbde3c6c39723d3a2335b69fccc0b0dad07593a
Vasya Korghikov <vakzn@yandex.ru>
2021-03-08 20:15:54 +0300

    <создание и детализация ошибок при первом написании. Версия: 1.0>

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+	var a: Int = 16
+	var b: Int = 16
+	println(a - b)
+	// здесь математика чистая
+}
+
+fun figa() {
+	val d: String = "Это барахло"
+	println(d) 
+}
+// это почему то не работает
+// но почему, не знаю
