6b1908a5d7020eb9a309846ba1104de8e678e8d8
Vasya Korghikov <71185420+Vakzn@users.noreply.github.com>
2021-03-11 16:14:20 +0300

    Upgrade Main.kt
    New code

---
+++ Main.kt
@@ -0,0 +1,14 @@
+fun main() {
+	var a: Int = 16
+	var b: Int = 16
+	println(a - b)
+	// треш какой то
+	// у меня в vs code отображается дичь
+}
+
+fun figa() {
+	val d: String = "Вот это лохотрон"
+	println(d) 
+}
+
+
